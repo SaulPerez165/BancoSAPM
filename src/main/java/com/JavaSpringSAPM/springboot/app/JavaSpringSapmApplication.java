4dc7a986363ecb07ef8d91f080f5095ac7411403
SaulPerez165 <perezsaul165@gmail.com>
2023-07-17 16:41:18 -0600

    Initial commit

---
+++ src/main/java/com/JavaSpringSAPM/springboot/app/JavaSpringSapmApplication.java
@@ -0,0 +1,13 @@
+package com.JavaSpringSAPM.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringSapmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringSapmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JavaSpringSAPM/springboot/app/JavaSpringSapmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaSpringSAPM.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringSapmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
