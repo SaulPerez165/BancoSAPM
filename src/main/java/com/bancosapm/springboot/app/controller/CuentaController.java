900816153da248af216e441677c5cdcaafb69143
SaulPerez165 <perezsaul165@gmail.com>
2023-07-18 23:02:21 -0600

    V2 E10 Crear el controlador
    Crear otro paquete para posteriormente crear el Controller del DAO.

---
+++ src/main/java/com/bancosapm/springboot/app/controller/CuentaController.java
@@ -0,0 +1,25 @@
+package com.bancosapm.springboot.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bancosapm.springboot.app.models.dao.ICuentaDao;
+
+@Controller
+public class CuentaController {
+
+	@Autowired
+	private ICuentaDao cuentaDao;
+
+	@RequestMapping(value="/lista", method = RequestMethod.GET)
+	public String cuentaLista(Model model) {
+		model.addAttribute("titulo", "Lista de cuentas");
+		model.addAttribute("cuentas", cuentaDao.findAll());
+		return "lista";
+	}
+
+}
